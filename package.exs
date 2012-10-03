6486563ce3c02a4d0b35463c66a9d4a3ed5ed404
Gleb Peregud <gleber.p@gmail.com>
2012-10-03 10:49:57 +0200

    add expm package

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "erlsemver", description: "Semantic Versioning parsing library",
+                 version: "1.0.0", keywords: ["semver", "string", "versioning"], 
+                 maintainers: [[name: "Gleb Peregud", 
+                                email: "gleber.p@gmail.com"]],
+                 repositories: [[github: "gleber/erlsemver"]])
+    
